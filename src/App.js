353b9548c2e4e1a5a90c71f7da5a3b11931f4d17
Biokarl <arturkuvaev4041997@gmail.com>
2022-09-02 15:24:53 +0300

    Добавил scss и компонент Home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import "./App.scss";
+import { Home } from "./pages/Home/Home";
 
 function App() {
-  return <div className="App">1</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Home.module.scss";
+export const Home = () => {
+  return (
+    <div className={styles.root}>
+      <div>1</div>
+      <div>2</div>
+    </div>
+  );
+};
